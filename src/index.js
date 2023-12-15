1e1eb1ffbfd9872196abdd9bf795eea9ac5d8e32
Paul Silvius <paulsilvius0@gmail.com>
2023-12-15 14:16:51 -0500

    Top selling albums outline added in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
-import React from "react"
-import ReactDom from "react-dom/client";
+import React from 'react';
+import ReactDom from 'react-dom/client';
 
-function Greeting() {
-    return <h2>Component #1</h2>
+const AlbumList = () =>{
+  return (
+    <section>
+      <Album />
+      <Album />
+      <Album />
+      <Album />
+    </section>
+  )
 }
 
-export default Greeting;
-const root = ReactDom.createRoot(document.getElementById('root'))
+const Album = () =>{
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Artist />
+    </article>
+  )
+}
 
-root.render(<Greeting />);
+const Image = () => <img 
+  src='https://content.discogs.com/media/Lana-Del-Rey-%E2%80%93-Did-You-Know-That-Theres-A-Tunnel-Under-Ocean-Blvd.jpeg'
+  alt='Did You Know That There’s A Tunnel...'
+  />
+const Title = () => <h2>Did You Know That There's A Tunnel Under Ocean Blvd</h2>
+const Artist = () => {
+  return <h4>Lana Del Rey</h4>;
+}
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+
+root.render(<AlbumList />);
